3d07233e83efdee9bebcdb4a05f82908a791db49
timothy833 <ca9m8e5zy@gmail.com>
2024-12-12 01:03:19 +0800

    final 3-3

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -171,21 +171,18 @@ insert into "COACH_LINK_SKILL" (coach_id, skill_id) values
     -- 1. 教練`肌肉棒子` 的經驗年數為3年
     -- 2. 教練`Q太郎` 的經驗年數為5年
 
-update "COACH" 
- set experience_years = 3
- where user_id =(
-	select id 
-	from "USER" 
-	where  email = 'muscle@hexschooltest.io'
- );
+update "COACH"
+set experience_years = 3
+from "USER"
+where "USER".email = 'muscle@hexschooltest.io'
+and "USER".id = "COACH".user_id;
 
-update "COACH" 
- set experience_years = 5
- where user_id =(
-	select id 
-	from "USER" 
-	where  email = 'starplatinum@hexschooltest.io'
- );
+update "COACH"
+set experience_years = 5
+from "USER"
+where "USER".email = 'starplatinum@hexschooltest.io'
+  and "USER".id = "COACH".user_id;
+
 
 -- 3-4 刪除：新增一個專長 空中瑜伽 至 SKILL 資料表，之後刪除此專長。
  insert into "SKILL" (name) values ('空中瑜伽');
