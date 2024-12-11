eb9d1691d504d40ceca1e6402be79815346ce708
timothy833 <ca9m8e5zy@gmail.com>
2024-12-12 02:06:28 +0800

    update 6-5

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -400,3 +400,8 @@ and purchase_at  <= '2024-11-30 00:00:00';
 -- 6-5. 查詢：計算 11 月份有預約課程的會員人數（需使用 Distinct，並用 created_at 和 status 欄位統計）
 -- 顯示須包含以下欄位： 預約會員人數
 
+select count(distinct "COURSE_BOOKING".user_id) as "預約會員人數"
+from "COURSE_BOOKING" 
+where created_at >= '2024-11-01 00:00:00' 
+and created_at <= '2024-11-30 00:00:00' 
+and status  != '課程已取消';
